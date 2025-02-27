e99488844cd50b91a2c83a51f56e1e77b4fb2123
jolymmii <yerbolkassymov27@gmail.com>
2025-02-27 16:04:55 +0500

    исправил момент с главной страницы

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainLayout from './layouts/MainLayout'
 import Home from './pages/Home'
 import './scss/app.scss'
+
 const Cart = React.lazy(
 	() => import(/* webpackChunkName: "Cart" */ './pages/Cart')
 )
@@ -16,18 +17,14 @@ const NotFound = React.lazy(
 
 function App() {
 	return (
-		<BrowserRouter basename='/react-pizza'>
-			{' '}
-			{/* Здесь добавляем basename */}
-			<Routes>
-				<Route path='/' element={<MainLayout />}>
-					<Route path='' element={<Home />} />
-					<Route path='*' element={<NotFound />} />
-					<Route path='cart' element={<Cart />} />
-					<Route path='pizza/:id' element={<FullPizzas />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<Routes>
+			<Route path='/' element={<MainLayout />}>
+				<Route path='' element={<Home />} />
+				<Route path='cart' element={<Cart />} />
+				<Route path='pizza/:id' element={<FullPizzas />} />
+				<Route path='*' element={<NotFound />} />
+			</Route>
+		</Routes>
 	)
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import store from './redux/store'
 
@@ -10,10 +10,10 @@ const rootElem = document.getElementById('root')
 if (rootElem) {
 	const root = ReactDOM.createRoot(rootElem)
 	root.render(
-		<BrowserRouter>
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
