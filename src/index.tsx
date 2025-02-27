f307c8054b7dc7fe948d2e4dbcd7a9725635e62b
jolymmii <yerbolkassymov27@gmail.com>
2025-02-27 16:22:09 +0500

    исправил некоторые моменты

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom' // Здесь мы используем HashRouter
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import store from './redux/store'
 
@@ -10,10 +10,10 @@ if (rootElem) {
 	const root = ReactDOM.createRoot(rootElem)
 	root.render(
 		<Provider store={store}>
-			<HashRouter basename='/react-pizza'>
+			<BrowserRouter basename='/react-pizza'>
 				{' '}
 				<App />
-			</HashRouter>
+			</BrowserRouter>
 		</Provider>
 	)
 }
